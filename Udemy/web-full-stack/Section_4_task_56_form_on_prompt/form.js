4d66cabe18eb3f6b7130aedfc40bbeed779440b4
Roman B <rvbem@yandex.ru>
2020-11-11 09:26:01 +0400

    udemy webfs sec4tsk56

---
+++ Udemy/web-full-stack/Section_4_task_56_form_on_prompt/form.js
@@ -0,0 +1,5 @@
+var Name = prompt("Введите имя");
+var Surname = prompt("Введите фамилию");
+var Age = prompt("Введите возраст");
+console.log("Пользователь: " + Name + " " + Surname);
+console.log("Возраст: " + Age);
