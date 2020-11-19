0e369323377156cfb390d9bdc611ebf94ef42f65
Roman B <rvbem@yandex.ru>
2020-11-19 09:04:39 +0400

    udemy webfs sec4tsk79

---
+++ Udemy/web-full-stack/Section_4_task_73_loops/scripts/loop.js
@@ -0,0 +1,12 @@
+/*
+var cnt = 30;
+while (cnt > 0) {
+    console.log(cnt);
+    cnt-=2;
+}
+*/
+
+for (i = 30; i >= 0; i-=2){
+        console.log(i);
+}
+
---
+++ Udemy/web-full-stack/Section_4_task_79_functions/scripts/function.js
@@ -0,0 +1,37 @@
+/*
+var cnt = 30;
+while (cnt > 0) {
+    console.log(cnt);
+    cnt-=2;
+}
+for (i = 30; i >= 0; i-=2){
+        console.log(i);
+}
+*/
+/*
+function isNumberOdd (num) {
+    if(num % 2 == 0){
+        console.log(num % 2);
+        console.log(true);
+    } else {
+        console.log(num % 2);
+        console.log(false);
+    }
+}
+isNumberOdd(7);
+*/
+
+function factorial (num){
+    var result = 1;
+    if (num == 0){
+        console.log(result);
+    } else {
+        var cnt = 1;
+        for (cnt = 1; cnt <= num; cnt++){
+            result = result * cnt;
+        }
+        console.log("res "+ result);
+    }
+}
+factorial(7);
+
